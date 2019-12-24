e7d38f479d99d45df7302be69ea42701dad04bd8
Mikhail Grachev <work@mgrachev.com>
2019-12-24 19:10:08 +0300

    Update entrypoint.sh
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 reek --single-line ${INPUT_REEK_FLAGS} \
-  | /bin/reviewdog -f=reek -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | /bin/reviewdog -efm="%*\s%f:%l: %m" -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"

f6792da73aa11373ceb85d9a89e1fa0e58591df6
Alireza Bashiri <me@alirezabashiri.com>
2020-05-06 19:30:46 +0700

    fix: typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 gem install -N reek $(version $INPUT_REEK_VERSION)
 
 reek --single-line ${INPUT_REEK_FLAGS} \
-  | /bin/reviewdog -f=reek -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+  | reviewdog -f=reek -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"

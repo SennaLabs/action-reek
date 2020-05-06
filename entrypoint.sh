948a5b2432f6111a6b823f8ca17b423dc79d1d6e
Alireza Bashiri <me@alirezabashiri.com>
2020-05-06 20:17:54 +0700

    fix: debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 gem install -N reek $(version $INPUT_REEK_VERSION)
 
-reek --single-line app/**/*.rb
+reek --single-line app/**/*.rb | reviewdog -f=reek -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
